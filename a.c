20c7ac9296606564b7c25bc0e80b0341fe924f0c
joshuamv7 <joshua.moreira7@gmail.com>
2021-06-12 21:56:56 -0600

    cambio funion

--- a.c
+++ a.c
@@ -6,9 +6,9 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb/stb_image_write.h"
 
-int main(void) {
+int filtrxor(char* name,int xor) {
      int width, height, channels;
-     unsigned char *img = stbi_load("2.png", &width, &height, &channels, 0);
+     unsigned char *img = stbi_load(name, &width, &height, &channels, 0);
      if(img == NULL) {
          printf("Error in loading the image\n");
          exit(1);
@@ -21,15 +21,23 @@ int main(void) {
          exit(1);
      }
  
-     // Sepia filter coefficients from https://stackoverflow.com/questions/1061093/how-is-a-sepia-tone-created
      for(unsigned char *p = img, *pg = sepia_img; p != img + img_size; p += channels, pg += channels) {
-         *pg       = (uint8_t)fmin(*p ^ 40 , 255.0);         // red
-         *(pg + 1) = (uint8_t)fmin(*p ^ 40 , 255.0);         // green
-         *(pg + 2) = (uint8_t)fmin(*p ^ 40 , 255.0);         // blue        
+         *pg       = (uint8_t)fmin(*p ^ xor , 255.0);         // red
+         *(pg + 1) = (uint8_t)fmin(*p ^ xor , 255.0);         // green
+         *(pg + 2) = (uint8_t)fmin(*p ^ xor , 255.0);         // blue        
          if(channels == 4) {
              *(pg + 3) = *(p + 3);
          }
      }
- 
+     sleep(1);
+        
      stbi_write_jpg("sky_sepia.jpg", width, height, channels, sepia_img, 100);     
+}
+
+void main(void){
+    char* a ="a";
+
+    // strcat(a,"xor");
+    puts(a);
+    // filtrxor("2.png",255);
 }
